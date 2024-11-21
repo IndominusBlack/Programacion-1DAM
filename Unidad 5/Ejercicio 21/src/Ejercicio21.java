17229bc631657eae9aa2df1c303db1081715a53c
IndominusBlack <eduheras2005@gmail.com>
2024-11-21 14:23:50 +0100

    Add files via upload

--- Unidad 5/Ejercicio 21/src/Ejercicio21.java
+++ Unidad 5/Ejercicio 21/src/Ejercicio21.java
@@ -2,12 +2,34 @@ import java.util.Scanner;
 
 public class Ejercicio21 {
     public static void main(String[] args) throws Exception {
-        
-        Scanner sc = new Scanner (System.in);
 
-        for (int intento = 1) {
-            System.out.println ("Introduce un número: ");
-            int numero = sc.nextInt();
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Introduce un número: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        int acumuladoraPar = 0;
+        int acumuladoraImpar = 0;
+        int acumuladoraMedia = 0;
+        int acumuladoraTotal = 0;
+        
+        while (num>=0){
+            acumuladoraTotal ++;
+            if (num %2 != 0) {
+            acumuladoraImpar += num;
+            acumuladoraMedia ++;
+            } else {
+            if (num > acumuladoraPar) {
+            acumuladoraPar = num;
+            }
+            }
+        System.out.println("Introduce un número: ");
+        num = sc.nextInt();
         }
-    }
+
+        int mediaImpares = acumuladoraImpar/acumuladoraMedia;
+        System.out.println("La media de los números impares es: " + mediaImpares);
+        System.out.println("El mayor número par introducido es: " + acumuladoraPar);
+        System.out.println("El número total de números introducidos es: " + acumuladoraTotal);
+}
 }
