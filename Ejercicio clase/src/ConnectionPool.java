a4f0b7219d3b8ba1e75a39d6473ad160d42c5114
EduardoHeras <eduardoheras04@gmail.com>
2025-02-17 11:32:42 +0100

    Add files via upload

---
+++ Ejercicio clase/src/ConnectionPool.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.util.ArrayList;
+
+public class ConnectionPool {
+    private ArrayList<Connection> conn;
+    private String url;
+    private String usuario;
+    private String contraseña;
+
+    public connectionPool(){
+
+    }
+}
